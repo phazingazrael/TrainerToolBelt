8d4b60e6a13f3640d22b578f6885ccb10606f343
phazingazrael <phazdverse@gmail.com>
2019-12-08 16:29:34 -0700

    got rendering on the new team window to show all options in the pokedex. Need to locate sprites for the missing minidex and discern how regional variant will be displayed

--- client/src/modules/NewTeam/NewTeam.jsx
+++ client/src/modules/NewTeam/NewTeam.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Button, Header, Card, Modal } from 'semantic-ui-react'
+import { Button, Header, Card, Modal, Grid } from 'semantic-ui-react'
 import Pokedex from "../dexImport";
 
 const NewTeam = () => (
   <Modal trigger={<Button>New Team </Button>} centered={false}>
     <Modal.Header>Select a Photo</Modal.Header>
     <Modal.Content image>
-      <Card>
-      <Card image={"./img/dex/"+Math.floor(Math.random() * 803)+".gif"} />
-      </Card>
-      <Modal.Description>
-        <Header>Select first team member</Header>
-          <Pokedex/>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <Card image={"./img/dex/"+Math.floor(Math.random() * 803)+".gif"} />
+          </Grid.Column>
+          <Grid.Column width={13}>
+            <Header>Select first team member</Header>
+            <Pokedex/>
+            <p>Is it okay to use this photo?</p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Modal.Content>
   </Modal>
 )
--- client/src/modules/dexImport/index.js
+++ client/src/modules/dexImport/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
 class Pokedex extends React.Component {
   constructor() {
@@ -11,9 +12,9 @@ class Pokedex extends React.Component {
   componentDidMount() {
     this.getDex()
       .then(res => {
-        console.log("receiving pokedex");
-        console.log(res);
-        this.setState({ pokedex: res});
+        //console.log("receiving pokedex");
+        //console.log(res);
+        this.setState({ pokedex: JSON.parse(res)});
       })
       .catch(err => console.log(err));
   }
@@ -30,8 +31,29 @@ class Pokedex extends React.Component {
     return body;
     };
 
-  render() {
-    return( <div>{this.state.pokedex}</div>);
+    render() {
+      console.log("dex " + JSON.stringify(this.state.pokedex));
+      this.state.pokedex.sort(function (a, b) {
+        return (a.national_id > b.national_id) ? 1 : ((b.national_id > a.national_id) ? -1 : 0);
+      });
+      const DexList = this.state.pokedex.map(function(item){
+        
+        //if (item.national_id > 0 && item.national_id < 10) {
+        //  item.national_id = "00"+ item.national_id;
+        //}else if (item.national_id > 10 && item.national_id < 100){
+        //  item.national_id = "0"+ item.national_id;
+        //}
+        return (
+          
+          <Card key={item.national_id} id={item.national_id+"-"+item.names.en} image={"./img/miniDex/"+item.national_id+".png"}/>
+        ) ;
+      });    
+  
+      return (
+          <Card.Group itemsPerRow={6}>
+            {DexList}
+          </Card.Group> 
+      );
   }
 }
 
