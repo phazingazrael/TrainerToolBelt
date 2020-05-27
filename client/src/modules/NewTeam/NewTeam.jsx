fd1e4f3a1cf112b3ece89cdd780bd7f7f027270a
Cameren Dessert <PhazeDVerse@gmail.com>
2020-05-27 13:13:25 -0600

    modified:   client/src/modules/NewTeam/NewTeam.jsx

--- client/src/modules/NewTeam/NewTeam.jsx
+++ client/src/modules/NewTeam/NewTeam.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import { Button, Header, Card, Modal, Grid } from 'semantic-ui-react'
+import React from "react";
+import { Button, Header, Card, Modal, Grid } from "semantic-ui-react";
 import Pokedex from "../dexImport";
 
 const NewTeam = () => (
-  <Modal id="newTeamModal" trigger={<Button>New Team </Button>} centered={false}>
+  <Modal
+    id="newTeamModal"
+    trigger={<Button>New Team </Button>}
+    centered={false}
+  >
     <Modal.Header>Select your starter</Modal.Header>
     <Modal.Content>
       <Grid>
         <Grid.Row>
           <Grid.Column width={3}>
-            <Card image={"./img/dex/"+Math.floor(Math.random() * 803)+".gif"} />
+            <Card
+              image={"./img/dex/" + Math.floor(Math.random() * 803) + ".gif"}
+            />
           </Grid.Column>
           <Grid.Column width={13} id="contentHeight">
             <Header id="newTeamModalHeader">Select first team member</Header>
-            <Pokedex/>
+            <Pokedex />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row className='buPar'>
-          <Button.Group floated='right'>
-            <Button attached='right' color='green'>Save Team</Button>
-            <Button color='red'>Cancel</Button>
-          </Button.Group>
+        <Grid.Row className="buPar">
+          <div>
+            <Button.Group floated="right">
+              <Button color="green">Save Team</Button>
+              <Button color="red">Cancel</Button>
+            </Button.Group>
+          </div>
         </Grid.Row>
       </Grid>
     </Modal.Content>
   </Modal>
-)
+);
 
-export default NewTeam
+export default NewTeam;
