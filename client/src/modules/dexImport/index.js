882197d32f8815a9c1ba9e264cc3fa83666332bc
Cameren Dessert <PhazeDVerse@gmail.com>
2020-05-27 19:33:37 -0600

    w

--- client/src/modules/dexImport/index.js
+++ client/src/modules/dexImport/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 class Pokedex extends React.Component {
   constructor() {
     super();
-    this.state = { 
+    this.state = {
       pokedex: [],
-      style:{
-        height:''
-      }
+      style: {
+        height: "",
+      },
     };
   }
 
   componentDidMount() {
     this.getDex()
-      .then(res => {
+      .then((res) => {
         console.log(res);
         var PokeDex = JSON.parse(res);
         //console.log(PokeDex);
@@ -23,64 +23,64 @@ class Pokedex extends React.Component {
         PokeDex.sort((a, b) => a.id - b.id);
         //sorting end
         //alert(JSON.parse(res));
-        this.setState({ pokedex: PokeDex});
+        this.setState({ pokedex: PokeDex });
       })
-      .catch(err => console.log(err));
-      this.calcHeight();
+      .catch((err) => console.log(err));
+    this.calcHeight();
   }
-
 
   getDex = async () => {
     const location = window.location.hostname;
     const response = await fetch(`http://${location}:5000/api/pokedex`, {
-      method: "GET"
+      method: "GET",
     });
     const body = await response.text();
 
     if (response.status !== 200) throw Error(body.message);
-    console.log("getDex " +body);
+    console.log("getDex " + body);
     return body;
   };
 
-  calcHeight =() =>{
+  calcHeight = () => {
     let winHeight = window.innerHeight;
     let modalHeight = document.getElementById("newTeamModal").clientHeight;
-    let differentiate = (winHeight - modalHeight)/2;
+    let differentiate = (winHeight - modalHeight) / 2;
     let headHeight = document.getElementById("newTeamModalHeader").clientHeight;
-    let newHeight = (((winHeight - modalHeight) + differentiate)/2)-10  +"px";
-    let contHeight = (newHeight+headHeight) + "px";
-    this.setState(prevState => {
-      let style = Object.assign({}, prevState.style);   // creating copy of state variable style
-      style.height = newHeight; 
-      console.log(style.height)                        // update the name property, assign a new value                 
-      return { style };                                 // return new object style object
+    let newHeight = (winHeight - modalHeight + differentiate) / 2 - 10 + "px";
+    let contHeight = newHeight + headHeight + "px";
+    this.setState((prevState) => {
+      let style = Object.assign({}, prevState.style); // creating copy of state variable style
+      style.height = newHeight;
+      console.log(style.height); // update the name property, assign a new value
+      return { style }; // return new object style object
     });
     console.log(contHeight);
-  }
+  };
 
-    render() {
-      const DexList = this.state.pokedex.map(function(item){
-        return (
-          <Card key={item.id} className="dexItem">
-            <LazyLoadImage
-              width="50px"
-              alt={item.name}
-              effect="blur"
-              src={"./img/miniDex/"+item.id+"-opt.png"} // use normal <img> attributes as props
-              />
-              <br/>
-              <p>
-                {item.name}
-              </p>
-          </Card>
-        ) ;
-      });    
-  
+  render() {
+    const DexList = this.state.pokedex.map(function (item) {
       return (
-          <Card.Group itemsPerRow={6} className="dexList" style={this.state.style}>
-            {DexList}
-          </Card.Group> 
+        <Card 
+        key={item.id} 
+        className="dexItem"
+        meta={item.type_1}
+        description={item.name}>
+          <LazyLoadImage
+            width="50px"
+            alt={item.name}
+            effect="blur"
+            src={"./img/miniDex/" + item.id + "-opt.png"} // use normal <img> attributes as props
+          />
+          <br />
+        </Card>
       );
+    });
+
+    return (
+      <Card.Group itemsPerRow={6} className="dexList" style={this.state.style}>
+        {DexList}
+      </Card.Group>
+    );
   }
 }
 
